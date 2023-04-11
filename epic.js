ef640725849cd19652bc61b3172533ae87b2c086
DINESHfing <99419646+DINESHfing@users.noreply.github.com>
2023-04-11 15:36:32 +0530

    v

--- epic.js
+++ epic.js
@@ -4,13 +4,21 @@ function login() {
   const vv = document.querySelector('input[name="username"]')[0];
 
   // Check if the username and password are correct
-  if (username === "vv" && password === "1") {
+  if (username === "gyan" && password === "1") {
     // Store the username and password in local storage
     localStorage.setItem("username", username);
     localStorage.setItem("password", password);
 
     // Redirect the user to the home page
     window.location.href = "DetailsPage/second.html";
+  }
+  if (username === "gyan" && password === "2") {
+    // Store the username and password in local storage
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+
+    // Redirect the user to the home page
+    window.location.href = "nxtpage1/index.html";
   } else {
     // Display an error message
     const errorMessage = document.getElementById("error-message");
